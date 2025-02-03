7b3d049b9dccfeb0f06d2cb98e56985369c3066a
DUB55 <tcookdub5@gmail.com>
2025-02-03 22:59:52 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import ThemeContext from './contexts/ThemeContext';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { FileExplorer } from './components/FileExplorer';
 import { FileViewer } from './components/FileViewer';
+ import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
   return (
